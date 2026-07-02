e576695ed892f873aa8e65179ad3901d12a50dd9
soloever2-maker <soloever2@gmail.com>
2026-07-02 16:06:12 +0300

    Delete template directory

--- template/auth/supabase/index.ts
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck
-export * from './hook';
-export * from './service';
-export * from './router';
-export * from './context';
--- template/core/index.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-// Simplified Core module exports
-export * from './types';
-export { configManager, createConfig } from './config';
-export { 
-  getSharedSupabaseClient, 
-  getSharedSupabaseClient as getSupabaseClient,
-  safeSupabaseOperation 
-} from './client';
